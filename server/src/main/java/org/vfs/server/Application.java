3ca7d1334c84e413e7180fa020b9d5a00fa59d38
Nikita Lipatov <nikita.lipatov@gmail.com>
2014-08-04 00:54:46 +0400

    develop: todo list was added

--- server/src/main/java/org/vfs/server/Application.java
+++ server/src/main/java/org/vfs/server/Application.java
@@ -4,6 +4,14 @@ import java.io.IOException;
 
 /**
  * Entry point
+ * TODO: maven release plugin:
+    + remove snapshot version and convert snapshot version to release version;
+    + merge into master branch;
+    + increase release version and convert release version to snapshot version;
+   TODO: spring-develop branch (after maven-release-plugin)
+    + class Server should be injected through spring injection system (xml configuration + annotations (PassScan?!))
+   // http://habrahabr.ru/post/231953/
+
  *
  * @author Lipatov Nikita
  */
